326199aa8c88b800e9a1187ab3acb52597067e5b
Andris Tsildermanis <yanich.2213@gmail.com>
2020-10-25 19:29:13 +0400

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello nuxt!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class Index extends Vue {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
