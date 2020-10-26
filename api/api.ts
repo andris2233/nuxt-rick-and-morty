af5988947154bb55f240321966c820920cf32be6
Andris Tsildermanis <tsildermanis@o-code.ru>
2020-10-26 10:41:46 +0400

    created first variation of api-service

---
+++ api/api.ts
@@ -0,0 +1,35 @@
+import http from './http';
+
+class Api {
+  base: string;
+  hasProps: string[];
+  constructor(base: string, hasProps: string[]) {
+    this.base = base;
+    this.hasProps = hasProps;
+  }
+  async getById (id: number | string): Promise<object> {
+    const res = await http.get(`${this.base}/${id}`);
+    return res;
+  };
+  async getAll(): Promise<object> {
+    const res = await http.get(this.base);
+    return res;
+  }
+  async getByFiltrer(props: any): Promise<object> {
+    const isValidProps: boolean = !!Object.keys(props).find((prop: string) => { !this.hasProps.includes(prop) });
+    if (!isValidProps) {
+      throw new Error('Invalid props!');
+    }
+    const propsStr: string = Object.keys(props).reduce((url: String, curProp: string): string => {
+      return url += `&${curProp}=${props[curProp]}`;
+    });
+    const res = await http.get(`${this.base}/?${propsStr.substr(1, propsStr.length)}`);
+    return res;
+  }
+}
+
+const character: Api = new Api('character', ['name', 'status', 'species', 'type', 'gender']);
+const location: Api = new Api('location', ['name', 'type', 'dimension']);
+const episode: Api = new Api('episode', ['name', 'episode']);
+
+export { character, location, episode };
